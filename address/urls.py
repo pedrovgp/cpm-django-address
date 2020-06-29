c273ce208691912ca81fc237182207e670803b60
pedrovgp <pedrovgp@gmail.com>
2020-06-29 08:18:54 -0300

    ADD re_path in urls

--- address/urls.py
+++ address/urls.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'address'
 urlpatterns = [
-    url(
+    re_path(
         regex=r'^address/add$',
         view=views.AddressCreateView.as_view(),
         name='address-create-view'
